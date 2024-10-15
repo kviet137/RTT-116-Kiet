9303a7dfbc94c246178d8508851e3214ea99b148
kviet137 <kviet137@gmail.com>
2024-10-15 11:25:03 -0400

    added try-catch cases for quantity of products.

--- Module-303/Module-303/src/coffee_shop/CoffeeShop.java
+++ Module-303/Module-303/src/coffee_shop/CoffeeShop.java
@@ -5,31 +5,22 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CoffeeShop {
-
-    // we will use this is a couple functions to read input from the user
     private Scanner scanner = new Scanner(System.in);
-
-    // this is a class level variable that will contain our list of products for sale
-    // best practice is to define these at the top of the class
     private List<Product> products = new ArrayList<>();
-
-    // this will hold the products that we are going to purchase
     private List<Product> cart = new ArrayList<>();
 
 
     private void initProducts() {
         Product p1 = new Product("Small Coffee", 4.57, 0);
         products.add(p1);
-
         Product p2 = new Product("Large Coffee", 7.99, 0);
         products.add(p2);
-
         Product p3 = new Product("Sugar Cookie", 5.89, 0);
         products.add(p3);
-
         Product p4 = new Product("Egg Sandwich", 6.49, 0);
         products.add(p4);
     }
+
 
     private void printProductMenu() {
         for (int count = 0; count < products.size(); count++) {
@@ -38,10 +29,10 @@ public class CoffeeShop {
             // before using it to create the string that is printed
             System.out.println((count + 1) + ") " + p.getName() + " \t " + p.getPrice());
         }
-
         // give some white space after print
         System.out.println("");
     }
+
 
     private int printMainMenu() {
         System.out.println("1) See product menu");
@@ -63,6 +54,7 @@ public class CoffeeShop {
         }
     }
 
+
     public void addProductToCart() {
         // 1 display the items for sale
         printProductMenu();
@@ -77,53 +69,42 @@ public class CoffeeShop {
         // we want to check that the user has entered a valid product number
         if (selection >= 1 && selection <= products.size()) {
             System.out.print("How many would you like to add?:");
-            int quantity = scanner.nextInt();
-            scanner.nextLine();
-
+            int quantity;
+            try {
+                quantity = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Invalid amount" + e.getMessage());
+                return;
+            } finally {
+                scanner.nextLine();
+            }
             Product p = products.get(selection - 1);
+
+            if (!cart.contains(p)) {
+                cart.add(p);
+            }
+
             if (quantity >=0) {
                 quantity = p.getQuantity() + quantity;
                 p.setQuantity(quantity);
-            }
-
-            else{
+            } else{
                 System.out.println("Please enter a valid quantity");
             }
-
-
-            // 3 add to the cart array
-            // we are subtracting 1 from the user input to get the real position in the array
-            // because most people do not have a concept of the 0th item in a list
-
-            for (int i = 0; i < quantity; i++) {
-                if (cart.contains(p)) {
-                    continue;
-                }else{
-                    cart.add(p);
-                }
-
-
-            }
             System.out.println("Added " + p.getQuantity() + " " + p.getName() + " to your cart.\n");
-
         } else {
             System.out.println("Invalid item selection");
         }
-
     }
 
     public void checkout() {
         System.out.println("==== Items in your cart ====");
 
         // list the items in the cart
-      double subtotal = 0.0;
+        double subtotal = 0.0;
         for (Product item : cart) {
             System.out.println(item.getName() + "\tx" + item.getQuantity() + " \t $" + item.getPrice());
             subtotal = subtotal + item.getPrice() * item.getQuantity();
         }
-
-
-
         System.out.println("");
         System.out.println("Subtotal\t\t $" + subtotal);
 
@@ -140,7 +121,7 @@ public class CoffeeShop {
 
     public void start() {
         // this becomes similar to the main method in that it will be where our project starts and runs
-        // 1) initialize the products for sale
+        // 1) initialize the products for saleki
         initProducts();
 
         // repeat forever until the user enters selection 4 which will exit the program
@@ -163,7 +144,6 @@ public class CoffeeShop {
                 checkout();
             } else if (selection == 4) {
                 System.out.println("Good bye");
-
                 // we are exiting with a value of 0 means successful exit
                 // this ends the program
                 System.exit(0);
@@ -171,7 +151,6 @@ public class CoffeeShop {
                 System.out.println("Invalid command entered " + selection + "\n");
             }
         }
-
     }
 
     public static void main(String[] args) {
